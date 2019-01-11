4eb6171dd82d29fe22f2d5bc24163e6c2ed96446
rene <renegmed@yahoo.com>
2019-01-11 16:08:53 -0500

    1-tab based: push and pop screens

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -33,11 +33,21 @@ class TextScreen extends Component {
           { Platform.OS === 'android' && <Button title='Show Tab Bar' testID={testIDs.SHOW_BOTTOM_TABS_BUTTON} onPress={() => this.toggleTabBarVisibility('BottomTabs', true)} /> }
           <Button title='Hide Tab Bar on Push' testID={testIDs.HIDE_BOTTOM_TABS_ON_PUSH_BUTTON} onPress={() => this.hideTabBarOnPush()} />
           <Button title='Show Left Side Menu' testID={testIDs.SHOW_LEFT_SIDE_MENU_BUTTON} onPress={() => this.showSideMenu('left')} />
-          <Button title='Show Right Side Menu' testID={testIDs.SHOW_RIGHT_SIDE_MENU_BUTTON} onPress={() => this.showSideMenu('right')} />
+          <Button title='Show Right Side Menu' testID={testIDs.SHOW_RIGHT_SIDE_MENU_BUTTON} onPress={() => this.showSideMenu('right')} /> 
+          <Button title='Push and Pop' testID={testIDs.PUSH_AND_POP_BUTTON} onPress={this.onClickPushPop} />
         </View>
       </Bounds>
     );
   } 
+  
+  onClickPushPop = async () => {
+    await Navigation.push(this.props.componentId, {
+      component: {
+        name: 'navigation.playground.PushedScreen'
+      }
+    });
+  }
+ 
 
   showSideMenu(side) { 
     Navigation.mergeOptions(this.props.componentId, {
