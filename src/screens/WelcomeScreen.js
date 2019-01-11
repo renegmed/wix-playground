fe8a37746d4a3c1a0c0e7b2bc6a5da667686b47c
rene <renegmed@yahoo.com>
2019-01-11 12:41:00 -0500

    set navigation defaults, activate Welcome page topbar

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import { View, Text  }  from 'react-native';
 
 class WelcomeScreen extends Component { 
+    static options() {
+        return {
+            _statusBar: {
+                backgroundColor: 'transparent',
+                style: 'dark',
+                drawBehind: true
+            },
+            topBar: {
+                title: {
+                    text: 'My Screen'
+                },
+                largeTitle: {
+                    visible: false,
+                },
+                drawBehind: true,
+                visible: true,
+                animate: false
+            }
+        };
+    }
+    
     render() {
         return (
             <View>
