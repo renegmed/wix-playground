c6e0e7a5306c482be0787e6d8b363ca77ec470a5
rene <renegmed@yahoo.com>
2019-01-11 18:49:11 -0500

    4-static lifecycle event: called Navigation.showOverlay method (not working)

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,7 @@
 import { Navigation } from 'react-native-navigation';
 
 export function registerScreens() {
-    Navigation.registerComponent('navigation.playground.WelcomeScreen', () => require('./WelcomeScreen').default);
+    Navigation.registerComponent('navigation.playground.WelcomeScreen', () => require('./WelcomeScreen').default); 
+    Navigation.registerComponent('navigation.playground.StaticLifecycleOverlay', () => require('./StaticLifecycleOverlay').default);
 }    
  
